7ce8e2e011158899b57b29fbc2d49574cf5acd58
ziqiqiiii <ziqi.thong@gmail.com>
2026-02-20 12:13:47 +0800

    Init: init Kheehub Android App
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/example/kheehub/MainActivity.java
    #	app/src/main/res/layout/activity_main.xml
    #	app/src/main/res/values-night/themes.xml
    #	app/src/main/res/values/strings.xml
    #	app/src/main/res/values/themes.xml
    #	gradle/libs.versions.toml
    #	gradle/wrapper/gradle-wrapper.properties
    #	gradlew.bat

--- app/src/main/java/com/example/kheehub/MainActivity.java
+++ app/src/main/java/com/example/kheehub/MainActivity.java
@@ -1,35 +1,24 @@
 package com.example.kheehub;
 
 import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
-        // Load map by default
-        loadFragment(new MapFragment());
-
-        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
-        bottomNav.setOnItemSelectedListener(item -> {
-            Fragment fragment;
-            int id = item.getItemId();
-            if (id == R.id.nav_map)           fragment = new MapFragment();
-            else if (id == R.id.nav_add)      fragment = new AddFragment();
-            else                              fragment = new SettingsFragment();
-            loadFragment(fragment);
-            return true;
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
         });
-    }
-
-    private void loadFragment(Fragment fragment) {
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.fragment_container, fragment)
-                .commit();
     }
 }
