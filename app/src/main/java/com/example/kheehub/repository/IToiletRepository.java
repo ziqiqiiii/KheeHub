08bc7e18213e7b0453a1456aded2cd77be229fce
jobpurii <job104142@gmail.com>
2026-04-04 04:19:26 +0800

    feat: show map, show toilet location, interact pin location, update toilet.

--- app/src/main/java/com/example/kheehub/repository/IToiletRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.kheehub.repository;
-
-import androidx.lifecycle.LiveData;
-import com.example.kheehub.model.Toilet;
-import java.util.List;
-
-public interface IToiletRepository {
-    LiveData<List<Toilet>> getToilets();
-}
